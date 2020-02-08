9e43e3b9c611cfd49b34cbf8b826f81713c406ae
ivan386 <ivan386@users.noreply.github.com>
2020-02-08 17:01:43 +0300

    use part of data

--- rc4.lua
+++ rc4.lua
@@ -1,19 +1,21 @@
 
-function rc4(key, data)
+function rc4(key, data, first, last)
 	-- lua 5.3.4
+	first = (first or 1) - 1
+	last = last or #data
 	
 	local j, s = 0, {}
 	for i = 0, 255 do
 		j = 255 & j + (s[i] or i) + key:byte(i % #key + 1)
-		s[i], s[j] = (s[j] or j), (s[i] or i)
+		s[i], s[j] = s[j] or j, s[i] or i
 	end
 	
 	local j, i, out = 0, 0, {}
-	for p = 1, #data do
+	for p = 1, last - first do
 		i = 255 & p
 		j = 255 & j + s[i]
 		s[i], s[j] = s[j], s[i]
-		table.insert(out, data:byte(p) ~ s[255 & s[i] + s[j]])
+		table.insert(out, data:byte(p + first) ~ s[255 & s[i] + s[j]])
 	end
 	
 	return string.char(table.unpack(out))
